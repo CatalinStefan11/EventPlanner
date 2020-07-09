c74b105176c1e10fca240f3b76c1329292927a2a
Catalin <catalin.stefann11@gmail.com>
2020-07-09 23:21:14 +0300

    google maps implemented2

--- app/src/main/java/ro/ase/eventplanner/Util/Util.java
+++ app/src/main/java/ro/ase/eventplanner/Util/Util.java
@@ -3,9 +3,9 @@ package ro.ase.eventplanner.Util;
 public class Util {
 
     public final static String EMPTY_STRING = "";
-    public final static String MESSAGE_HISTORY_ENDPOINT = "http://10.0.2.2:8080/%s/%s/";
-    public final static String MISSED_CONVERSATIONS_ENDPOINT = "http://10.0.2.2:8080/%s/";
-    public final static String WEBSOCKET_ENDPOINT = "ws://10.0.2.2:8080/websocket";
+    public final static String MESSAGE_HISTORY_ENDPOINT = "http://52.15.203.44/%s/%s/";
+    public final static String MISSED_CONVERSATIONS_ENDPOINT = "http://52.15.203.44/%s/";
+    public final static String WEBSOCKET_ENDPOINT = "ws://52.15.203.44/websocket";
 
     public static String removeEndlines(String message) {
         return message.replaceAll("(\\r|\\n|\\r\\n)+", "\\\\n");
